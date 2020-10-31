14e68021fae564fb1be93541ab97a61403464703
kyedonghwan-linux <kdh643737@gmail.com>
2020-10-31 23:58:53 +0900

    20201031-2

---
+++ core/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class TimeStampedModel(models.Model):
+    """ TimeStamped Model """
+
+    created = models.DateTimeField()
+    updated = models.DateTimeField()
+
+    class Meta:
+        abstract = True
