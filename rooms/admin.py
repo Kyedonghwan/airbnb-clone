573b3a4b29340b596a675146eafc615a7cb99f46
kyedonghwan-linux <kdh643737@gmail.com>
2020-11-02 12:41:39 +0900

    20201102

--- rooms/admin.py
+++ rooms/admin.py
@@ -15,7 +15,54 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition"""
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        (
+            "More About the Spaces",
+            {"fields": ("amenities", "facilities", "house_rules")},
+        ),
+        ("Spaces", {"fields": ("beds", "bedrooms", "baths", "guests")}),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "price",
+        "beds",
+        "bedrooms",
+        "baths",
+        "guests",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    search_fields = ("city", "^host__username")
+
+    filter_horizontal_ = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
+
+    def count_amenities(self, obj):
+        return obj.amenities.count()
 
 
 # Register your models here.
