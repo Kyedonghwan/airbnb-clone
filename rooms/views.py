a1d63a6c0b19f6ce25b0ebbe17fe5672203e2641
kyedonghwan-linux <kdh643737@gmail.com>
2020-11-06 11:39:26 +0900

    20201106

--- rooms/views.py
+++ rooms/views.py
@@ -1,20 +1,38 @@
-from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.views.generic import ListView
 from . import models
 
 # Create your views here.
 
 
-def all_rooms(request):
-    page = request.GET.get("page")
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    return render(request, "rooms/home.html", {"rooms": rooms})
+class HomeView(ListView):
+
+    """ HombeView Defnition"""
+
+    model = models.Room
+    paginate_by = 10
+    ordering = "created"
+    paginate_orphans = 5
+    page_kwarg = "page"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
 
 
 """
+def all_rooms(request):
+    page = request.GET.get("page",1)
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10, orphans=5)
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", {"page": rooms})
+    except EmptyPage:
+        return redirect("/")
+
+
+
 def all_rooms(request):
     
     page = request.GET.get("page", 1)
