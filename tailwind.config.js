c5d82e346d50b73f0ce259c134f7ce8cbc6324a9
kyedonghwan-linux <kdh643737@gmail.com>
2020-12-31 20:01:21 +0900

    20201231

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        "h-25vh": "25vh",
+        "h-50vh": "50vh",
+        "h-75vh": "75vh"
+      }
+    },
   },
   variants: {
     extend: {},
